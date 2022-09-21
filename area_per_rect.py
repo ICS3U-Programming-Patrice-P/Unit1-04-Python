edc21b44e1995469b4a7daa25b95e1a3913722c7
Patrice Pat-Odita <patrice.pat-odita@stu.ocsb.ca>
2022-09-21 17:18:39 +0000

    Added area and perimeter

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Patrice Pat-Odita
+# Created on: Sep. 20, 2022
+# This program calculates and displays the area and perimeter of a rectangle with length 6cm and width 8cm.
+
+def main():
+    print ("For a rectangle that has dimensions")
+    print ("length 6cm and width 8cm:")
+    print ("")
+    print ("Area = {} cm^2". format (6*8))
+    print ("Perimeter = {} cm". format (2*(6+8)))
+
+
+if __name__ == "__main__":
+  main()
---
+++ basic_math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Patrice Pat-Odita
+# Created on: Sep. 19, 2
+# This program does basic math.
+
+def main():
+    print ("5 + 4 -1 = {}". format(5+4-1))
+    print ("40 / 2 * * 3 = {}". format(40/2**3))
+    print ("56 % 5 = {}". format(56%5))
+  
+if __name__ == "__main__":
+  main()
+ 
